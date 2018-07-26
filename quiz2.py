babeeab2674f711d50ceed946eb345d9a97070e0
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2018-07-26 19:47:57 +0900

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -12,11 +12,4 @@ def solution(arr):
             answer.append(arr[i])
     answer.append(arr[len(arr)-1])
     
-        
-            
-            
-    
-    
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    #print('Hello Python')
     return answer
