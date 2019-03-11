3c033169c2016cda0aafaa3f434abc40a642bae2
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-03-11 10:45:12 +0900

    Create top.py

---
+++ Stack&Queue/top.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42588?language=python3
+def solution(heights):
+    len_h= len(heights)
+    answer = []
+    heights.reverse()
+    for i in range(len_h-1):#i=0~len_h-2 
+        check=0
+        for j in range(i+1,len_h):#j=i+1,...,len_h-1
+            if check==0 and heights[i]<heights[j]:
+                check=1
+                answer.append(len_h-j)
+        if check==0:
+            answer.append(0)
+    answer.append(0)
+    answer.reverse()
+    return answer
