9992d86addc2579a7fbe69e7e7ee7f5ed27991d8
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-09-08 01:08:20 +0900

    Create 루시와엘라찾기.sql

---
+++ 7Days_SQL_Training/DAY07/루시와엘라찾기.sql
@@ -0,0 +1,5 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59046
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
